43832bcaf7776e6a50d6f07083bc683a55858824
Yuva PHALLE <yuva.phalle@hacking-lab.com>
2023-02-02 17:45:41 +0100

    updating flag script

--- root/flag-deploy-scripts/deploy-env-flag.sh
+++ root/flag-deploy-scripts/deploy-env-flag.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/with-contenv bash
+#!/command/with-contenv bash
 # Use this script to write / replace the goldnugget to the correct place.
 # The goldnugget is available as $GOLDNUGGET.
 
